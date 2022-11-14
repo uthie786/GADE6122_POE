f336b89c4abdbb2fcd481529214458f8b53cef21
Uthmaan Essack <112628327+uthie786@users.noreply.github.com>
2022-11-14 22:35:06 +0200

    Completed up to 2.2

---
+++ Gade6122_POE/Gade6122_Part1_corrected/MeleeWeapon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gade6122_Part1_corrected
+{
+    public enum Types { Dagger, Longsword}
+    internal class MeleeWeapon : Weapon
+    {
+        public MeleeWeapon(Types type, int x, int y) : base(x, y, '⚔')
+        {
+            switch (type)
+            {
+                case Types.Dagger:
+                    weaponType = "Dagger";
+                    durability = 10;
+                    dmg = 3;
+                    cost = 3;
+                    break;
+                case Types.Longsword:
+                    weaponType = "Longsword";
+                    durability = 6;
+                    dmg = 4;
+                    cost = 5;
+                    break;
+                default:
+
+                    break;
+            }
+        }
+
+    }
+}
---
+++ Gade6122_POE/Gade6122_Part1_corrected/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gade6122_Part1_corrected
+{
+    internal class Weapon : Item
+    {
+        protected int dmg;
+        protected int range;
+        protected int durability;
+        protected int cost;
+        protected string weaponType;
+
+        public int DMG { get { return dmg; } set { dmg = value; } }
+        public virtual int Range { get { return range; } set { range = value; } }
+        public int Durability { get { return durability; } set { durability = value; } }
+        public int Cost { get { return cost; } set { cost = value; } }
+        public string WeaponType { get { return weaponType; } set { weaponType = value; } }
+
+        public Weapon(int x, int y, char symbol) : base(x, y)
+        {
+
+        }
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
